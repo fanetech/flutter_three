f1b0175fe2a9442f226615459a5dc65597fd1f50
fanetech <louguefranck.20@gmail.com>
2024-01-10 18:03:15 +0000

    sliverAppbar

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:lecle_flutter_carousel_pro/lecle_flutter_carousel_pro.dart';
+import 'package:test_app/widgets/sliverBar.dart';
+// import 'package:another_carousel_pro/another_carousel_pro.dart';
 
 void main() => runApp(MyApp());
 
@@ -92,75 +95,154 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Form(
-        key: formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              value,
-              style: TextStyle(fontSize: 20, color: Colors.deepPurple),
-              textAlign: TextAlign.center,
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                label: Text('Name'),
-                hintText: 'Enter your name',
-                icon: Icon(Icons.person),
-              ),
-              keyboardType: TextInputType.text,
-              autocorrect: true,
-              // onChanged: change,
-              validator: (val) =>
-                  (val!.isEmpty || val == null) ? 'check your name' : null,
-              onSaved: (value) => name = value!,
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: "phone",
-                hintText: "Telephone",
-                icon: Icon(
-                  Icons.phone,
-                  color: Colors.blue,
-                  size: 25,
-                ),
-              ),
-              keyboardType: TextInputType.number,
-              // onChanged: change2,
-              autofocus: true,
-              autocorrect: true,
-              validator: (val) => val!.isEmpty ? 'check your phone' : null,
-              onSaved: (value) => phone = value!,
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: "password",
-                hintText: "password",
-                icon: Icon(
-                  Icons.password,
-                  color: Colors.blue,
-                  size: 25,
-                ),
-              ),
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              // onChanged: change3,
-              // autofocus: true,
-              // autocorrect: true,
-              validator: (val) => val!.isEmpty ? 'check your password' : null,
-              onSaved: (value) => password = value!,
-            ),
-            ElevatedButton(
-              onPressed: () => submit(name),
-              child: const Text('Submit'),
-            ),
-          ],
-        ),
-      ),
+      body: SliverBar(),
+      // body: ListView(
+      //   children: [
+      //     // const CarouselPage(),
+      //     Form(
+      //       key: formKey,
+      //       // child: CarouselPage(),
+      //       child: Column(
+      //         mainAxisAlignment: MainAxisAlignment.center,
+      //         children: [
+      //           Text(
+      //             value,
+      //             style: TextStyle(fontSize: 20, color: Colors.deepPurple),
+      //             textAlign: TextAlign.center,
+      //           ),
+      //           TextFormField(
+      //             decoration: const InputDecoration(
+      //               label: Text('Name'),
+      //               hintText: 'Enter your name',
+      //               icon: Icon(Icons.person),
+      //             ),
+      //             keyboardType: TextInputType.text,
+      //             autocorrect: true,
+      //             // onChanged: change,
+      //             validator: (val) =>
+      //                 (val!.isEmpty || val == null) ? 'check your name' : null,
+      //             onSaved: (value) => name = value!,
+      //           ),
+      //           TextFormField(
+      //             decoration: const InputDecoration(
+      //               labelText: "phone",
+      //               hintText: "Telephone",
+      //               icon: Icon(
+      //                 Icons.phone,
+      //                 color: Colors.blue,
+      //                 size: 25,
+      //               ),
+      //             ),
+      //             keyboardType: TextInputType.number,
+      //             // onChanged: change2,
+      //             autofocus: true,
+      //             autocorrect: true,
+      //             validator: (val) => val!.isEmpty ? 'check your phone' : null,
+      //             onSaved: (value) => phone = value!,
+      //           ),
+      //           TextFormField(
+      //             decoration: const InputDecoration(
+      //               labelText: "password",
+      //               hintText: "password",
+      //               icon: Icon(
+      //                 Icons.password,
+      //                 color: Colors.blue,
+      //                 size: 25,
+      //               ),
+      //             ),
+      //             keyboardType: TextInputType.text,
+      //             obscureText: true,
+      //             // onChanged: change3,
+      //             // autofocus: true,
+      //             // autocorrect: true,
+      //             validator: (val) =>
+      //                 val!.isEmpty ? 'check your password' : null,
+      //             onSaved: (value) => password = value!,
+      //           ),
+      //           ElevatedButton(
+      //             onPressed: () => submit(name),
+      //             child: const Text('Submit'),
+      //           ),
+      //         ],
+      //       ),
+      //     ),
+      //   ],
+      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+// class CarouselPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: SizedBox(
+//           height: 150.0,
+//           width: 300.0,
+//           child: AnotherCarousel(
+//             boxFit: BoxFit.cover,
+//             autoplay: false,
+//             animationCurve: Curves.fastOutSlowIn,
+//             animationDuration: Duration(milliseconds: 1000),
+//             dotSize: 6.0,
+//             dotIncreasedColor: Color(0xFFFF335C),
+//             dotBgColor: Colors.transparent,
+//             dotPosition: DotPosition.bottomCenter,
+//             dotVerticalPadding: 10.0,
+//             showIndicator: true,
+//             indicatorBgPadding: 7.0,
+//             images: [
+//               NetworkImage(
+//                   'https://i.pinimg.com/originals/a2/4e/29/a24e29fc5ee1ef69ab11777f1d28641a.jpg'),
+//               NetworkImage(
+//                   'https://i.pinimg.com/originals/93/a0/ae/93a0aeb22c7e27da4796efb4b2297ea4.jpg'),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class CarouselPage extends StatelessWidget {
+  const CarouselPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.2,
+          width: MediaQuery.of(context).size.width * 0.7,
+          child: const Carousel(
+            hasBorderRadius: true,
+            boxFit: BoxFit.cover,
+            autoplay: false,
+            animationCurve: Curves.fastOutSlowIn,
+            animationDuration: Duration(milliseconds: 1000),
+            dotSize: 6.0,
+            dotIncreasedColor: Color(0xFFFF335C),
+            dotBgColor: Colors.transparent,
+            dotPosition: DotPosition.topRight,
+            dotVerticalPadding: 10.0,
+            dotColor: Colors.grey,
+            showIndicator: true,
+            indicatorBgPadding: 7.0,
+            images: [
+              NetworkImage(
+                  'https://docs.flutter.dev/assets/images/flutter-logo-sharing.png'),
+              NetworkImage(
+                  'https://docs.flutter.dev/assets/images/flutter-logo-sharing.png'),
+              // ExactAssetImage('assets/images/LaunchImage.jpg'),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/widgets/sliverBar.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class SliverBar extends StatefulWidget {
+  const SliverBar({super.key});
+
+  @override
+  State<SliverBar> createState() => _SliverBarState();
+}
+
+class _SliverBarState extends State<SliverBar> {
+  Widget resto(String menu, Color color, String image) {
+    return Container(
+      color: color,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset(image),
+          Text(menu),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          title: Text(
+            'Welcome to my app',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          pinned: true,
+          floating: false,
+          expandedHeight: 200,
+          backgroundColor: Colors.transparent,
+          flexibleSpace: FlexibleSpaceBar(
+            background: Image.asset(
+              'images/test.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SliverFixedExtentList(
+          itemExtent: 150,
+          delegate: SliverChildListDelegate([
+            resto('Menu 1', Colors.red, 'images/test.jpg'),
+            resto('Menu 2', Colors.blue, 'images/test.jpg'),
+            resto('Menu 3', Colors.green, 'images/test.jpg'),
+            resto('Menu 4', Colors.yellow, 'images/test.jpg'),
+            resto('Menu 5', Colors.purple, 'images/test.jpg'),
+            resto('Menu 5', Colors.purple, 'images/test.jpg'),
+            resto('Menu 5', Colors.purple, 'images/test.jpg'),
+            resto('Menu 5', Colors.purple, 'images/test.jpg'),
+            resto('Menu 5', Colors.purple, 'images/test.jpg'),
+            resto('Menu 5', Colors.purple, 'images/test.jpg'),
+            resto('Menu 5', Colors.purple, 'images/test.jpg'),
+          ]),
+        )
+      ],
+    );
+  }
+}
